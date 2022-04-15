985ec5bbde48f4e0cf147542cf91b8c1baea7ef8
i-am-sam-codes <sammmceja@gmail.com>
2022-04-15 16:48:35 -0700

    Add Calculator class

--- app.js
+++ app.js
@@ -3,14 +3,62 @@ const operatorBtns = document.querySelectorAll('[data-operator]')
 const equalsBtn = document.querySelector('[data-equals]')
 const deleteBtn = document.querySelector('[data-delete]')
 const allClearBtn = document.querySelector('[data-all-clear]')
-const previousOperandText = document.querySelector('[data-previous-operand]')
-const currentOperandText = document.querySelector('[data-current-operand]')
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
 
+class Calculator {
 
+    constructor(previousOperand, currentOperand) {
+        this.previousOperand = previousOperand
+        this.currentOperand = currentOperand
+        
+    }
 
+    clear () {
+        this.previousOperand = ''
+        this.currentOperand = ''
+        this.operand = undefined
+
+    }
+
+    delete () {
+
+    }
+
+    appendNum (number) {
+        this.currentOperand = number
+
+    }
+
+    updateDisplay () {
+        this.currentOperand = this.currentOperand
+    }
+ 
+    add(a, b) {
+        return a + b
+    }
+
+    sub(a, b) {
+        return a - b
+    }
+
+    div(a, b) {
+        if (b == 0) throw new Error('div by 0 not allowed');
+        return a / b
+    }
+
+    multiply(a, b) {
+        return a * b
+    }
+}
+
+const calculator = new Calculator(previousOperand, currentOperand)
+
+// Button click event
 numberBtns.forEach(button => {
     button.addEventListener('click', () => {
-        .appendNumber(button.textContent) 
-        .updateDisplay()
+        calculator.appendNum(button.textContent)
+        calculator.updateDisplay()
     })
 })
+
