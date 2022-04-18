6b3d730602aee66909d60a2c128d136050e59dcb
i-am-sam-codes <sammmceja@gmail.com>
2022-04-18 16:42:11 -0700

    Add functionality

--- app.js
+++ app.js
@@ -1,64 +1,26 @@
-const numberBtns = document.querySelectorAll('[data-number]')
-const operatorBtns = document.querySelectorAll('[data-operator]')
-const equalsBtn = document.querySelector('[data-equals]')
-const deleteBtn = document.querySelector('[data-delete]')
-const allClearBtn = document.querySelector('[data-all-clear]')
-const previousOperand = document.querySelector('[data-previous-operand]')
-const currentOperand = document.querySelector('[data-current-operand]')
+const result = document.getElementById('input')
 
-class Calculator {
+let calculate=(number) => {
+    result.value += number
+}
 
-    constructor(previousOperand, currentOperand) {
-        this.previousOperand = previousOperand
-        this.currentOperand = currentOperand
-        
+function Result() {
+    try {
+        result.value = eval(result.value)// checks if input value is a valid equation
     }
-
-    clear () {
-        this.previousOperand = ''
-        this.currentOperand = ''
-        this.operand = undefined
-
-    }
-
-    delete () {
-
-    }
-
-    appendNum (number) {
-        this.currentOperand = number
-
-    }
-
-    updateDisplay () {
-        this.currentOperand = this.currentOperand
-    }
- 
-    add(a, b) {
-        return a + b
-    }
-
-    sub(a, b) {
-        return a - b
-    }
-
-    div(a, b) {
-        if (b == 0) throw new Error('div by 0 not allowed');
-        return a / b
-    }
-
-    multiply(a, b) {
-        return a * b
+    catch(err) {
+        alert('👿Please enter a valid entry😈')
     }
 }
 
-const calculator = new Calculator(previousOperand, currentOperand)
+function clear() {
+    result.value = ' ';
+}
+let del=()=> {
+    result.value = result.value.slice(0, -1)
+}
 
-// Button click event
-numberBtns.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNum(button.textContent)
-        calculator.updateDisplay()
-    })
-})
+
+
+
 
